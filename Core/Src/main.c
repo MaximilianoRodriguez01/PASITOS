49cc266e6923f1e4ebb399b0cd5c3ffddb89d26e
felipeperassi <fperassi@fi.uba.ar>
2024-12-16 12:53:06 -0300

    adc

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -161,7 +161,7 @@ void SystemClock_Config(void)
     Error_Handler();
   }
   PeriphClkInit.PeriphClockSelection = RCC_PERIPHCLK_ADC;
-  PeriphClkInit.AdcClockSelection = RCC_ADCPCLK2_DIV6;
+  PeriphClkInit.AdcClockSelection = RCC_ADCPCLK2_DIV2;
   if (HAL_RCCEx_PeriphCLKConfig(&PeriphClkInit) != HAL_OK)
   {
     Error_Handler();
@@ -189,12 +189,12 @@ static void MX_ADC1_Init(void)
   /** Common config
   */
   hadc1.Instance = ADC1;
-  hadc1.Init.ScanConvMode = ADC_SCAN_DISABLE;
-  hadc1.Init.ContinuousConvMode = ENABLE;
+  hadc1.Init.ScanConvMode = ADC_SCAN_ENABLE;
+  hadc1.Init.ContinuousConvMode = DISABLE;
   hadc1.Init.DiscontinuousConvMode = DISABLE;
   hadc1.Init.ExternalTrigConv = ADC_SOFTWARE_START;
   hadc1.Init.DataAlign = ADC_DATAALIGN_RIGHT;
-  hadc1.Init.NbrOfConversion = 1;
+  hadc1.Init.NbrOfConversion = 2;
   if (HAL_ADC_Init(&hadc1) != HAL_OK)
   {
     Error_Handler();
@@ -202,9 +202,19 @@ static void MX_ADC1_Init(void)
 
   /** Configure Regular Channel
   */
-  sConfig.Channel = ADC_CHANNEL_0;
+  sConfig.Channel = ADC_CHANNEL_TEMPSENSOR;
   sConfig.Rank = ADC_REGULAR_RANK_1;
-  sConfig.SamplingTime = ADC_SAMPLETIME_71CYCLES_5;
+  sConfig.SamplingTime = ADC_SAMPLETIME_239CYCLES_5;
+  if (HAL_ADC_ConfigChannel(&hadc1, &sConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Configure Regular Channel
+  */
+  sConfig.Channel = ADC_CHANNEL_0;
+  sConfig.Rank = ADC_REGULAR_RANK_2;
+  sConfig.SamplingTime = ADC_SAMPLETIME_1CYCLE_5;
   if (HAL_ADC_ConfigChannel(&hadc1, &sConfig) != HAL_OK)
   {
     Error_Handler();
--- app/src/task_temperature.c
+++ app/src/task_temperature.c
@@ -28,7 +28,7 @@ float ADC_Ext_Temperature() {
 	uint16_t value;
 
 	if (HAL_OK == ADC_Poll_Read_Channel(&value, ADC_CHANNEL_1)) {
-		float temperature = ((float)value * 3.3 * 100.0) / 4096.0;
+		float temperature = ((float)value * 0.456 * 3.3 * 100.0) / 4096.0;
     	return temperature;
 	}
 
@@ -40,7 +40,7 @@ float ADC_Int_Temperature() {
     uint16_t value;
 
     if (HAL_OK == ADC_Poll_Read_Channel(&value, ADC_CHANNEL_TEMPSENSOR)) {
-        float temperature = ((((float)value * 3.3 / 4096.0) - V25) / AVG_SLOPE) + 25.0;
+        float temperature = ((((float)value * 0.456 * 3.3 / 4096.0) - V25) / AVG_SLOPE) + 25.0;
         return temperature;
     }
 
@@ -66,7 +66,7 @@ static HAL_StatusTypeDef ADC_Poll_Read_Channel(uint16_t *value, uint32_t channel
 
     sConfig.Channel = channel;
     sConfig.Rank = ADC_REGULAR_RANK_1;
-    sConfig.SamplingTime = ADC_SAMPLETIME_71CYCLES_5;
+    sConfig.SamplingTime = (channel == ADC_CHANNEL_TEMPSENSOR) ? ADC_SAMPLETIME_71CYCLES_5 : ADC_SAMPLETIME_1CYCLE_5;
 
     res = HAL_ADC_ConfigChannel(&hadc1, &sConfig);
     if (res != HAL_OK) {
